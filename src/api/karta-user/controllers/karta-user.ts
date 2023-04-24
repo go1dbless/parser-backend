54604825b685fbd1577b4299dd99f38a7285d4e7
Vlad Bondar <strongbondar@gmail.com>
2023-04-24 17:57:12 +0600

    add karta user

---
+++ src/api/karta-user/controllers/karta-user.ts
@@ -0,0 +1,7 @@
+/**
+ * karta-user controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::karta-user.karta-user');
---
+++ src/api/karta-user/services/karta-user.ts
@@ -0,0 +1,7 @@
+/**
+ * karta-user service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::karta-user.karta-user');
